ccb0dc452f35e4c5430c5bf0fb9a6fc6e31c633f
Matt Heath <matt@mattheath.com>
2013-02-05 10:36:53 +0000

    Add recipe for Colloquy IRC client

---
+++ attributes/colloquy.rb
@@ -0,0 +1,3 @@
+node.default["colloquy_download_uri"]="http://colloquy.info/downloads/colloquy-2.4.1.zip"
+node.default["colloquy_sha"]="b77fa4434603c6ad485d4475f24cd97458ee03a8fac026ff26e2a7532e5dbf0f"
+node.default["colloquy_app_path"]="/Applications/Colloquy.app"
